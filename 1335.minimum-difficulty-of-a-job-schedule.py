fcde82ba9bab62736eb4a9f4a12f6e304db789c8
jiehonglin <jiehonglin@kkcompany.com>
2023-12-29 17:51:15 +0800

    Refactor 1335.minimum-difficulty-of-a-job-schedule.py

--- 1335.minimum-difficulty-of-a-job-schedule.py
+++ 1335.minimum-difficulty-of-a-job-schedule.py
@@ -12,20 +12,20 @@ from typing import List
 class Solution:
     def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
         @cache
-        def f(i: int, budget: int, prev_max: int) -> int:
-            """Return best job difficulty consider following args as a state
+        def f(i: int, budget: int, curr_max: int) -> int:
+            """Return best job difficulty consider the following args as a state
 
             Args:
-                i (int): started from index i onwards for
+                i (int): started from index i onwards
                 budget (int): remaining budget days for arrangement
-                prev_max (int): job difficulty at last day
+                curr_max (int): job difficulty at current running day
 
             Returns:
                 int: min difficulty consider the given state
             """
             if i == n:
                 if budget == 0:
-                    return prev_max
+                    return curr_max
 
                 return math.inf
 
@@ -39,9 +39,9 @@ class Solution:
 
             # Two choices
             # END: use current task as starting task on a new day
-            end = prev_max + f(i + 1, budget - 1, jobDifficulty[i])
+            end = curr_max + f(i + 1, budget - 1, jobDifficulty[i])
             # MIXED: merge current task to last day
-            mix = f(i + 1, budget, max(prev_max, jobDifficulty[i]))
+            mix = f(i + 1, budget, max(curr_max, jobDifficulty[i]))
 
             return min(end, mix)
 
